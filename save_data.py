f7c68a3e68f751cac9b797cdc1dcdd42d6ce7699
26Mz <josef.meier@ksk.ch>
2024-12-17 12:17:02 +0100

    Update save_data.py

--- save_data.py
+++ save_data.py
@@ -1,24 +1,22 @@
 import serial
-import time
-import os
 
 # Pfad zur seriellen Schnittstelle (z. B. COM3 für Windows oder /dev/ttyUSB0 für Linux/Mac)
-port = "COM3"  # Passe dies an deinen Port an
-baud_rate = 9600  # Baudrate muss mit der im Arduino-Code übereinstimmen
+port = "COM3"  # Agjust to your port, seen in the Arduino IDE, if you click on "Arduino Uno" on the top left.
+baud_rate = 9600  # Must be same as serial.begin() in the .ino code
 output_file = "data\\arduino_data0.txt"
 
 try:
-    # Serielle Verbindung öffnen
+    # open serial connection
     ser = serial.Serial(port, baud_rate, timeout=1)
     print(f"Verbindung zu {port} hergestellt")
     
     with open(output_file, "w") as file:
         while True:
-            # Daten von der seriellen Verbindung lesen
+            # read data from serial connection
             line = ser.readline().decode('utf-8').strip()
             if line:
                 print(f"Gelesen: {line}")
-                file.write(line + "\n")  # Daten in die Datei schreiben
+                file.write(line + "\n")  # Write data to .txt document
 except KeyboardInterrupt:
     print("\nProgramm beendet")
 except Exception as e:
